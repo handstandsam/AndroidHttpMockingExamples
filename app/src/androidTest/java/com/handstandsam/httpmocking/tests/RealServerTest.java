52a75355f151cd7fbac2ceea7706d6c6b0560a09
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2015-12-08 12:28:13 -0500

    Moved Normal API test to own Test Class.

--- app/src/androidTest/java/com/handstandsam/httpmocking/tests/RealServerTest.java
+++ app/src/androidTest/java/com/handstandsam/httpmocking/tests/RealServerTest.java
@@ -5,13 +5,11 @@ import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.joshskeen.weatherview.BuildConfig;
 import com.joshskeen.weatherview.MainActivity;
 import com.joshskeen.weatherview.R;
-import com.joshskeen.weatherview.service.WeatherServiceManager;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -26,23 +24,18 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static com.handstandsam.httpmocking.util.AssetReaderUtil.asset;
 import static org.hamcrest.Matchers.containsString;
 
-@RunWith(AndroidJUnit4.class)
-public class WireMockTest extends
-        ActivityInstrumentationTestCase2<MainActivity> {
 
-    Logger logger = LoggerFactory.getLogger(WireMockTest.class);
+@RunWith(AndroidJUnit4.class)
+public class RealServerTest extends
+        ActivityInstrumentationTestCase2<MainActivity> {
+    Logger logger = LoggerFactory.getLogger(RealServerTest.class);
 
     private MainActivity activity;
 
-    public WireMockTest() {
+    public RealServerTest() {
         super(MainActivity.class);
     }
 
@@ -60,29 +53,19 @@ public class WireMockTest extends
         activity = getActivity();
     }
 
+
     /**
-     * Test WireMock
+     * Test Real API
      */
     @Test
-    public void testWiremock() {
-        logger.debug("testWiremock");
+    public void testRealAPI() {
+        logger.debug("testRealAPI");
 
-        WireMock.configureFor(BuildConfig.IP, BuildConfig.PORT);
         String city = "atlanta";
-        String jsonBody = asset(activity, city + "-conditions.json");
-        stubFor(get(urlMatching("/api/.*"))
-                .atPriority(5)
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withBody(jsonBody)));
-
-        String serviceEndpoint = "http://" + BuildConfig.IP + ":" + BuildConfig.PORT;
-        logger.debug("WireMock Endpoint: " + serviceEndpoint);
-        activity.setWeatherServiceManager(new WeatherServiceManager(serviceEndpoint));
-
         onView(ViewMatchers.withId(R.id.editText)).perform(typeText(city));
         onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.textView)).check(matches(withText(containsString("GA"))));
     }
+
 
 }
